c1b321dab473b8640bd4b6a9cce3ce388f15598e
배채겸 <bae-chaegyeom@baechaegyeoms-MacBook-Pro.local>
2020-12-02 23:51:28 +0900

    와인데이터베이스 필터링 테스트-Between

--- build/routes/wine.js
+++ build/routes/wine.js
@@ -4,4 +4,5 @@ var express_1 = require("express");
 var WineController_1 = require("../controllers/WineController");
 var router = express_1.Router();
 router.get('/', WineController_1.default.listAll);
+router.get('/filter', WineController_1.default.filteringWine);
 exports.default = router;
