271562d11ae11b62e895edb380711e8bff1785fd
배채겸 <bae-chaegyeom@baechaegyeoms-MacBook-Pro.local>
2020-12-10 22:29:44 +0900

    [Server] / Fix / maxAge 1시간 설정

--- build/app.js
+++ build/app.js
@@ -55,6 +55,7 @@ typeorm_1.createConnection()
             origin: [
                 'http://localhost:3000',
                 'http://54.180.150.63:3000',
+                'http://localhost:3001',
                 'http://buenosvinos-client.s3-website.ap-northeast-2.amazonaws.com'
             ],
             methods: ['GET', 'POST', 'OPTIONS', 'PATCH', 'DELETE'],
